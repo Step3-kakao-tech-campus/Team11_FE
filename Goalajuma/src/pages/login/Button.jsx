096dccb23a6d04b6ce8cd0728e8a692ea0b3243e
GimGayeon <kky1373@naver.com>
2023-09-18 15:44:20 +0900

    fix:app.jsx 경로수정 and loginBtn

--- Goalajuma/src/pages/login/Button.jsx
+++ Goalajuma/src/pages/login/Button.jsx
@@ -1,12 +1,16 @@
-const Button = ({children, onClick, disabled, className}) => {
-	return (
-		<button className = {className} disabled={disabled} onClick={(e) => {
-			e.preventDefault()
-			onClick(e)
-		}}>
-			{children}
-		</button>
-	)
-}
+const Button = ({ children, onClick, disabled, className }) => {
+  return (
+    <button
+      className={className}
+      disabled={disabled}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
