8b803e539aaebe076e021118881ceda7a1c532d2
munjihye <m84422583@gmail.com>
2023-09-21 02:35:40 +0900

    design: 로그인 페이지 전체 요소 배치 및 디자인 수정
    - 아직 요소 배치가 덜 끝나서 수정이 더 필요하다
    - material icon 설치

--- Goalajuma/src/components/login/Input.jsx
+++ Goalajuma/src/components/login/Input.jsx
@@ -1,6 +1,8 @@
+import styled from "styled-components";
+
 const Input = ({type, value, className, onChange, placeholder, id}) => {
   return (
-    <input 
+    <StyledInput 
       id={id}
       className={className}
       type={type} 
@@ -12,3 +14,10 @@ const Input = ({type, value, className, onChange, placeholder, id}) => {
 }
 
 export default Input;
+
+const StyledInput = styled.input`
+  width: 240px;
+  padding:15px 20px;
+  border-radius: 50px;
+  font-size: 15px;
+`
