6485828c154a1eb9f2713c46f0a0e6ddee577de7
GimGayeon <kky1373@naver.com>
2023-09-24 18:09:32 +0900

    feat:add alert and fix result

--- Goalajuma/src/components/home/ButtonLayout.jsx
+++ Goalajuma/src/components/home/ButtonLayout.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import MainButton from "../common/voteButton/MainButton";
 import styled from "styled-components";
+import Swal from "sweetalert2";
 
 //map 으로 버튼 받아온거 뿌려주기
 const ButtonLayout = ({ options, participants, isOwner }) => {
@@ -10,28 +11,46 @@ const ButtonLayout = ({ options, participants, isOwner }) => {
   const [choice, setChoice] = useState(options?.choice);
 
   const clickButton = (e) => {
-    setParticipant(true);
-    setChoice(e.target.id);
-
+    if (participant === true) {
+      Swal.fire({
+        icon: "info",
+        text: "투표를 수정하시겠습니까?",
+        showCancelButton: true,
+        confirmButtonText: "예",
+        cancelButtonText: "아니오",
+        confirmButtonColor: "#429f50",
+        cancelButtonColor: "#d33",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          setChoice(e.target.id);
+          //서버에 수정 요청 보내기
+        }
+      });
+    } else {
+      setParticipant(true);
+      setChoice(e.target.id);
+    }
     //이미 클릭된거는 클릭 막기.............
   };
   return (
-    <Container>
-      {optionList?.map((option) => {
-        return (
-          <MainButton
-            key={option.id}
-            id={option.id}
-            name={option.optionName}
-            value={option.optionRatio}
-            number={option.optionCount}
-            participant={participant}
-            clickButton={clickButton}
-            choice={choice}
-          />
-        );
-      })}
-    </Container>
+    <>
+      <Container>
+        {optionList?.map((option) => {
+          return (
+            <MainButton
+              key={option.id}
+              id={option.id}
+              name={option.optionName}
+              value={option.optionRatio}
+              number={option.optionCount}
+              participant={participant}
+              clickButton={clickButton}
+              choice={choice}
+            />
+          );
+        })}
+      </Container>
+    </>
   );
 };
 
--- Goalajuma/src/styles/Container.jsx
+++ Goalajuma/src/styles/Container.jsx
@@ -2,5 +2,6 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
   max-width: 393px;
+  height: 852px;
   padding: 0 48px;
 `;
