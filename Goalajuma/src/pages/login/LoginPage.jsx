c77c75a23390fc87691c87fc94bc8133b9ca5758
munjihye <m84422583@gmail.com>
2023-09-25 20:50:31 +0900

    feat: 회원가입 연결, 로그인 버튼 클릭 시 메인 화면

--- Goalajuma/src/pages/login/LoginPage.jsx
+++ Goalajuma/src/pages/login/LoginPage.jsx
@@ -4,14 +4,12 @@ import Button from "../../components/login/Button";
 import { useState } from "react";
 import {GoChevronLeft} from "react-icons/go"
 import {RiKakaoTalkFill} from "react-icons/ri"
+import { useNavigate } from "react-router-dom";
 
 
 const LoginPage = () => {
   const [value, setValue] = useState({email:"", password:""})
-  const handleClick = (e) => {
-    window.history.back();
-    console.log(e)
-  }
+  const navigate = useNavigate()
 
   const handleOnChange = (e) => {
     const {id, value} = e.target
@@ -21,14 +19,14 @@ const LoginPage = () => {
   return (
     <>
       <Header>
-        <StyledButton onClick={handleClick}> 
+        <StyledButton onClick={()=>navigate(-1)}> 
           <GoChevronLeft />
         </StyledButton>
         <Title>Goalajuma</Title>
       </Header>
       <Subheader>
         <span>계정이 없으신가요?</span>
-        <button>회원가입하기</button>
+        <button onClick={()=>{navigate("/signup")}}>회원가입하기</button>
       </Subheader>
       <Group>
         <InputGroup
@@ -51,7 +49,7 @@ const LoginPage = () => {
         />
       </Group>
       <Group>
-        <Button color="#9EB0EA">로그인</Button>
+        <Button color="#9EB0EA" onClick={()=>{navigate("/")}}>로그인</Button>
         <Button color="#FEE500"> <Icon><RiKakaoTalkFill style={{paddingRight: '8px', fontSize: '20px'}}/></Icon>카카오 로그인</Button>
       </Group>
     </>
@@ -71,8 +69,8 @@ const Title = styled.div`
   width: 100%;
   margin: 0 auto;
   position: relative;
-  right: 35px;
-  top: 3px;
+  right: 60px;
+
 `;
 const Header = styled.div`
   display: flex;
@@ -102,7 +100,9 @@ const Group = styled.div`
 `
 const StyledButton = styled.button`
   border: none;
-  padding: 10px;
+  position: relative;
+  bottom: 10px;
+  right: 30px;
   background: none;
   font-size:30px;
 `
