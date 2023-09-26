3c417e405c2f14c3bcc1e8148d2cc597c61b3151
GimGayeon <kky1373@naver.com>
2023-09-26 11:32:33 +0900

    fix:global container

--- Goalajuma/src/pages/main/MainPage.jsx
+++ Goalajuma/src/pages/main/MainPage.jsx
@@ -2,16 +2,23 @@ import HomeLayout from "../../components/home/HomeLayout";
 import { ButtonTest } from "../../components/home/ButtonTest";
 
 const MainPage = () => {
-  const data = ButtonTest.data;
+  const datas = ButtonTest.data.votes;
 
   /**
    * 무한스크롤 구현하기
    */
 
   return (
-    <div>
-      <HomeLayout data={data} options={data.options} />
-    </div>
+    <>
+      {" "}
+      {datas &&
+        datas.map((data) => (
+          <>
+            {" "}
+            <HomeLayout id={data.id} data={data} options={data.options} />
+          </>
+        ))}
+    </>
   );
 };
 
