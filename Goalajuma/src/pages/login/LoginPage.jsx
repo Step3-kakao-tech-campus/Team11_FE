661d74230c44181edf7c5b8dec50562d130bc13e
munjihye <m84422583@gmail.com>
2023-09-28 01:42:16 +0900

    design: err message desgin

--- Goalajuma/src/pages/login/LoginPage.jsx
+++ Goalajuma/src/pages/login/LoginPage.jsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import { GoChevronLeft } from "react-icons/go";
 import { RiKakaoTalkFill } from "react-icons/ri";
 import { useNavigate } from "react-router-dom";
+import {MainContainer} from '../../styles/Container';
+import useValid from "../../hooks/useValid";
 
 const LoginPage = () => {
   const [value, setValue] = useState({ email: "", password: "" });
@@ -15,12 +17,15 @@ const LoginPage = () => {
     setValue((prev) => ({ ...prev, [id]: value }));
   };
 
+  // 유효성 검사 text 반환을 위한 커스텀 훅 
+  const {validText, isValid} = useValid(value)
+
   return (
-    <>
+    <MainContainer>
       <Header>
-        <StyledButton onClick={() => navigate(-1)}>
+        <StyledIcon onClick={() => navigate(-1)}>
           <GoChevronLeft />
-        </StyledButton>
+        </StyledIcon>
         <Title>Goalajuma</Title>
       </Header>
       <Subheader>
@@ -35,14 +40,15 @@ const LoginPage = () => {
       </Subheader>
       <Group>
         <InputGroup
-          className="input"
           id="email"
           type="email"
           placeholder="이메일을 입력해주세요"
           label="Email"
           value={value.email}
+          valid={!isValid.isEmail}
           onChange={handleOnChange}
         />
+        <StyledEmailErr>{validText.emailText}</StyledEmailErr>
         <InputGroup
           className="input"
           id="password"
@@ -50,46 +56,47 @@ const LoginPage = () => {
           placeholder="******"
           label="비밀번호"
           value={value.password}
+          valid={!isValid.isPassword}
           onChange={handleOnChange}
         />
+        <StyledPasswordErr>{validText.passwordText}</StyledPasswordErr>
       </Group>
-      <Group>
+      <ButtonGroup>
         <Button
+          className="firstButton"
           color="#9EB0EA"
           onClick={() => {
             navigate("/");
           }}
+          disabled={isValid.isEmail && isValid.isPassword ? false : true}
         >
           로그인
         </Button>
         <Button color="#FEE500">
-          {" "}
           <Icon>
             <RiKakaoTalkFill
-              style={{ paddingRight: "8px", fontSize: "20px" }}
+              style={{ paddingRight: "8px", fontSize: "20px", position: "relative", top:'4px'}}
             />
+            카카오 로그인
           </Icon>
-          카카오 로그인
         </Button>
-      </Group>
-    </>
+      </ButtonGroup>
+    </MainContainer>
   );
 };
 
 export default LoginPage;
 
-const Icon = styled.i`
-  position: relative;
-  top: 4px;
+const Icon = styled.div`
+  color: #333;
 `;
 const Title = styled.div`
-  font-size: 30px;
+  font-size: 32px;
   font-weight: bold;
-  color: #9eb0ea;
-  width: 100%;
-  margin: 0 auto;
+  color: #9EB0EA;
   position: relative;
-  right: 60px;
+  bottom: 3px;
+  left: 25px;
 `;
 const Header = styled.div`
   display: flex;
@@ -103,24 +110,49 @@ const Subheader = styled.div`
   position: absolute;
   top: 120px;
   button {
-    margin-left: 30px;
+    margin-left: 40px;
     color: #9eb0ea;
+    border :none;
+    padding: 8px 15px;
+    border-radius: 50px;
   }
 `;
 const Group = styled.div`
   display: flex;
   flex-direction: column;
   gap: 30px;
-  .input {
-    position: relative;
-    bottom: 60px;
-  }
-`;
-const StyledButton = styled.button`
-  border: none;
+  align-items: center;
   position: relative;
-  bottom: 10px;
-  right: 30px;
+  top: 80px;
+`;
+const ButtonGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;  
+  align-items: center;
+  position: relative;
+  top: 100px;
+  .firstButton{
+    margin-top: 20px;
+  }
+`
+const StyledIcon = styled.button`
+  border: none;
   background: none;
-  font-size: 30px;
+  font-size:35px;
+  margin: 0 5px;
+`;
+const StyledErr = styled.div`
+  color: #e45151;
+  font-size: 13px;
+  position: absolute;
+  left: 20px;
+`;
+
+const StyledEmailErr = styled(StyledErr)`
+  top: 100px;
+`;
+
+const StyledPasswordErr = styled(StyledErr)`
+  top: 225px;
 `;
