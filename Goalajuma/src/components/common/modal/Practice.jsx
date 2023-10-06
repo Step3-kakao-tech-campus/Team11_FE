05cc6def99d81fd061895d909c588e79aeab2306
munjihye <m84422583@gmail.com>
2023-10-07 01:09:06 +0900

    feat: modal data 불러오기

---
+++ Goalajuma/src/components/common/modal/Practice.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import Modal from './Modal'
+import {ModalTest} from './ModalTest';
+import ModalLayout from './ModalLayout';
+// import HomeLayout from "../home/HomeLayout"
+// import { ButtonTest } from "../home/ButtonTest.js";
+import { HomeContainer } from "../../../styles/Container";
+import Button from '../../login/Button';
+
+const Practice = () => {
+  // 모달 보임 여부 결정 
+  const [modalVisible, setModalVisible] = useState(false) 
+  const data = ModalTest.data;
+  const id = ModalTest.data.id;
+
+  const openModal = () =>{
+    setModalVisible(true)
+  }
+
+  const closeModal = () => {
+    setModalVisible(false)
+  }
+
+  return (
+    <>
+      <Button onClick={openModal} color='red'>Open Modal</Button>
+      {
+        modalVisible && 
+        <Modal
+          visible={modalVisible}
+          closable={true}
+          maskClosable={true}
+          onClose={closeModal}>
+            <HomeContainer>
+              <ModalLayout id={id} data={data} what="main" />
+            </HomeContainer>
+        </Modal>
+      }
+    </>
+  )
+}
+
+export default Practice
