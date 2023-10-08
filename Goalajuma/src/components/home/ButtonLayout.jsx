1e6dea71f6dd77f025796cd0827146fe1ebf40d0
munjihye <m84422583@gmail.com>
2023-10-08 15:37:46 +0900

    fix: 모달 데이터 수정

--- Goalajuma/src/components/home/ButtonLayout.jsx
+++ Goalajuma/src/components/home/ButtonLayout.jsx
@@ -13,7 +13,6 @@ const ButtonLayout = ({ onClick, options, participate, isOwner, active }) => {
   //서버에서 받아온 value, number 값
 
   const optionList = options;
-
   //choice 하나만 선택할 수 있는 함수 구현해서 프롭스로 보내주기
 
   return (
