5f167f994c788820942bb00e990c07d339a6120e
munjihye <m84422583@gmail.com>
2023-10-21 13:41:59 +0900

    feat: 투표 종료 버튼 alert

--- Goalajuma/src/components/common/voteButton/EndButton.jsx
+++ Goalajuma/src/components/common/voteButton/EndButton.jsx
@@ -1,22 +1,39 @@
-import PropTypes from 'prop-types'
-import { useState } from 'react';
-import styled from 'styled-components'
+import PropTypes from "prop-types";
+import { useState,useEffect } from "react";
+import styled from "styled-components";
+import Alert from "../Alert";
 
 /**
  * @param {object} props
  * @param {boolean} props.isOwner 작성자 확인 
  * @param {string} props.active 투표 진행중 여부 : continue, complete
  */
-const EndButton = ({isOwner, active: initialActive, className}) => {
+const EndButton = ({isOwner, active: initialActive}) => {
   const [active, setActive] = useState(initialActive);
+  const [alert, setIsAlert] = useState(false);
+
+  useEffect(() => {
+    // active 상태가 업데이트될 때마다 이 효과가 실행됩니다.
+    console.log(active); // 업데이트된 active 값을 여기서 사용할 수 있습니다.
+  }, [active]); 
+  useEffect(() => {
+    // active 상태가 업데이트될 때마다 이 효과가 실행됩니다.
+    console.log(alert); // 업데이트된 active 값을 여기서 사용할 수 있습니다.
+  }, [alert]); 
 
   const handleOnClick = () => {
-    setActive("complete");
+    setActive("complete")
+    setIsAlert(true)
   };
 
-  if(isOwner && active==="continue") {
+  if(isOwner && active === "continue") {
     return (
-      <ButtonStyled onClick={handleOnClick}>끝내기</ButtonStyled>
+      <>
+        {alert && <Alert setIsAlert={setIsAlert}>
+          투표가 종료됩니다.
+        </Alert>}
+        <ButtonStyled onClick={handleOnClick}>끝내기</ButtonStyled>
+      </>
     )
   }
 }
