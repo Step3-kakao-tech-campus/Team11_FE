af7064b3ef42fddf2a13a274aeb7f985ae2d59b9
munjihye <m84422583@gmail.com>
2023-11-07 00:55:16 +0900

    feat: 댓글 조회 api 연결 시도

--- Goalajuma/src/components/common/modal/ModalLayout.jsx
+++ Goalajuma/src/components/common/modal/ModalLayout.jsx
@@ -6,21 +6,23 @@ import VoteBottom from "@/components/common/voteButton/VoteBottom";
 import ChatForm from "./ChatForm";
 import ChatWriteForm from "./ChatWriteForm";
 import styled from "styled-components";
-import { useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import Modal from "./Modal";
 import ShareForm from "./ShareForm";
 import { detailInquire } from "@/services/main";
 import { useQuery } from "@tanstack/react-query";
+import Loader from "@/assets/Loader";
 
 /**
  *
- * @param {object} data
+ * @param {object} id 
  * @param {string} what
  */
 
 const ModalLayout = ({ id, what }) => {
-  const { data } = useQuery({
+
+  const { data} = useQuery({
     queryKey: ["voteId"],
     queryFn: () => {
       console.log(id);
@@ -28,23 +30,11 @@ const ModalLayout = ({ id, what }) => {
     },
     enabled: !!id,
   });
-
-  const {
-    totalCount,
-    participate,
-    isOwner,
-    title,
-    content,
-    endDate,
-    active,
-    options,
-    username,
-    category,
-  } = data.data.data.vote;
-
-  const [participateState, setParticipate] = useState(participate);
+  const detailData = data?.data.data.vote;
+  console.log(detailData);
+  const [participateState, setParticipate] = useState(detailData.participate);
   const [modalVisible, setModalVisible] = useState(false);
-  // console.log(category);
+
   const shareCloseModal = () => {
     setModalVisible(false);
   };
@@ -57,24 +47,25 @@ const ModalLayout = ({ id, what }) => {
   };
 
   return (
+    <Suspense fallback={<Loader/>}>
     <MainContainer className="modal">
       <Container>
         <VoteHead
-        totalCount={totalCount}
-        endDate={endDate}
+        totalCount={detailData.totalCount}
+        endDate={detailData.endDate}
         what={what}
-        isOwner={isOwner}
-        active={active}
-        username={username}
-        categoryValue={category}
+        isOwner={detailData.isOwner}
+        active={detailData.active}
+        username={detailData.username}
+        categoryValue={detailData.category}
         ></VoteHead>
-        <MainContent title={title} content={content}></MainContent>
+        <MainContent title={detailData.title} content={detailData.content}></MainContent>
 
         <ButtonLayout
           participate={participateState}
-          isOwner={isOwner}
-          active={active}
-          options={options}
+          isOwner={detailData.isOwner}
+          active={detailData.active}
+          options={detailData.options}
           onClick={clickButton}
         ></ButtonLayout>
 
@@ -91,10 +82,11 @@ const ModalLayout = ({ id, what }) => {
         )}
       </Container>
       <Chat>
-        <ChatForm />
-        <ChatWriteForm participate={participate} />
+        <ChatForm id={id}/>
+        <ChatWriteForm participate={detailData.participate} />
       </Chat>
     </MainContainer>
+    </Suspense>
   );
 };
 const Container = styled.div`
