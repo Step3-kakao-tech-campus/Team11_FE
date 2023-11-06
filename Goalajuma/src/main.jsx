94bec05ff6805038695f75ed4d58e45d27c2ed93
munjihye <m84422583@gmail.com>
2023-11-07 01:51:18 +0900

    chore: cookie 설정

--- Goalajuma/src/main.jsx
+++ Goalajuma/src/main.jsx
@@ -7,9 +7,11 @@ import { RecoilRoot } from "recoil";
 import ScrollToTop from "./components/common/ScrollToTop";
 import GlobalStyle from "./styles/GlobalStyle";
 import { BrowserRouter } from "react-router-dom";
+import { CookiesProvider } from 'react-cookie';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <CookiesProvider>
       <RecoilRoot>
         <GlobalStyle />
         <BrowserRouter>
@@ -17,5 +19,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <App />
         </BrowserRouter>
       </RecoilRoot>
+    </CookiesProvider>
   </React.StrictMode>
 );
