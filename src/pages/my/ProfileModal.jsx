153d940250cf19b5881ee7eaa3b51f1db21f2d31
강성준 <comebacktoearth@gangseongjun-ui-MacBookAir.local>
2023-11-11 20:32:03 +0900

    개발 끝

--- src/pages/my/ProfileModal.jsx
+++ src/pages/my/ProfileModal.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Palette } from "@/styles/Palette";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import useValid from "@/hooks/useValid";
 import { useSetRecoilState } from "recoil";
@@ -9,7 +9,6 @@ import { newNameInquire, newEmailInquire } from "@/services/my";
 import Swal from "sweetalert2";
 import { removeToken } from "@/services/login";
 import { emailCheckInquire,nicknameCheckInquire } from "@/services/signup";
-import Input from "@/components/login/Input";
 
 /**
  *
@@ -19,10 +18,9 @@ import Input from "@/components/login/Input";
  * @returns
  */
 const ProfileModal = ({ myNickName, myEmail, img }) => {
-  const originInfo = { name: myNickName, email: myEmail };
   const [newInfo, setNewInfo] = useState({
-    name: myNickName,
-    email: myEmail,
+    name: "",
+    email: "",
     password: "",
   });
   const [input, setInput] = useState(false);
@@ -30,7 +28,7 @@ const ProfileModal = ({ myNickName, myEmail, img }) => {
   const [checkName, setCheckName] = useState(false);
   const [checkEmail, setCheckEmail] = useState(false);
   const setisLoginIn = useSetRecoilState(isLoginInState);
-
+  
   const handleMyInfo = () => {
     setInput((prev) => !prev);
   };
@@ -44,14 +42,12 @@ const ProfileModal = ({ myNickName, myEmail, img }) => {
       setCheckEmail(false)
     }
   };
+ 
 
   const handleSubmit = () => {
-    if (newInfo.name != originInfo.name) {
-      newNameInquire(newInfo.name);
-    }
-    if (newInfo.email != originInfo.email) {
-      newEmailInquire(newInfo.email);
-    }
+    newNameInquire(newInfo.name);    
+    newEmailInquire(newInfo.email);
+
     setInput((prev) => !prev);
     alert("저장되었습니다!");
   };
@@ -85,18 +81,22 @@ const ProfileModal = ({ myNickName, myEmail, img }) => {
     })
     .catch(err=>{
       console.log(err)
-      Swal.fire({
-        icon: "error",
-        text: "이미 사용중인 이름입니다.",
-        confirmButtonColor: "#d33",
-      })
+      if(newInfo.name === myNickName) {
+        Swal.fire({
+          icon: "success",
+          text: "사용가능한 이름 입니다!",
+          confirmButtonColor: "#429f50",
+          }) 
+          setCheckName(true);
+      } else {
+        Swal.fire({
+          icon: "error",
+          text: "이미 사용중인 이름입니다.",
+          confirmButtonColor: "#d33",
+        })
+      }
     })
   }
-  useEffect(() => {
-    console.log(              !isValid.isName ||
-      !isValid.isEmail ||
-      checkName && checkEmail)
-  },[isValid.isName, isValid.isEmail, checkName, checkEmail])
 
   const emailCheck=()=>{
     emailCheckInquire(newInfo.email)
@@ -104,24 +104,34 @@ const ProfileModal = ({ myNickName, myEmail, img }) => {
       setCheckEmail(true)
       Swal.fire({
         icon: "success",
-        text: "사용가능한 이메일 입니다!",
+        text: "사용가능한 이메일입니다!",
         confirmButtonColor: "#429f50",
         })
     })
     .catch(err=>{
       console.log(err)
-      Swal.fire({
-        icon: "error",
-        text: "이미 사용중인 이메일입니다.",
-        confirmButtonColor: "#d33",
-      })
+      if(newInfo.email === myEmail) {
+        Swal.fire({
+          icon: "success",
+          text: "사용가능한 이메일입니다!",
+          confirmButtonColor: "#429f50",
+          })
+          setCheckEmail(true);
+      } else {
+        Swal.fire({
+          icon: "error",
+          text: "이미 사용중인 이메일입니다.",
+          confirmButtonColor: "#d33",
+        })
+      }
     })
   }
-  console.log(isValid.isName, isValid.isEmail, checkName, checkEmail);
-  
+
   return (
     <div>
-      <Img src={`/image/${img}`} alt="사용자 프로필" />
+      <Img className="userImg">
+        <img src={`/image/${img}`} alt="사용자 프로" />
+      </Img>
       {!input && (
         <ProfileButton onClick={() => handleMyInfo()}>수정하기</ProfileButton>
       )}
@@ -130,21 +140,21 @@ const ProfileModal = ({ myNickName, myEmail, img }) => {
         <input
           type="text"
           id="name"
-          defaultValue={myNickName}
+          placeholder={input ? "" : myNickName}
           disabled={!input}
           onChange={handleOnChange}
         />
-        {input && <StyledButton onClick={nicknameCheck} disabled={!newInfo.name}>중복 검사</StyledButton>}
+        {input && <StyledButton onClick={nicknameCheck} disabled={!newInfo.name && !isValid.isName}>중복 검사</StyledButton>}
         <div className="error">{validText.nameText}</div>
         <label htmlFor="email">이메일</label>
         <input
           type="email"
           id="email"
-          defaultValue={myEmail}
+          placeholder={input ? "" :  myEmail}
           disabled={!input}
           onChange={handleOnChange}
         />
-        {input && <StyledButton onClick={emailCheck} disabled={!newInfo.email}>중복 검사</StyledButton>}
+        {input && <StyledButton onClick={emailCheck} disabled={!newInfo.email && !isValid.isEmail}>중복 검사</StyledButton>}
         <div className="error">{validText.emailText}</div>
       </InputBox>
       <ButtonBox>
@@ -152,9 +162,10 @@ const ProfileModal = ({ myNickName, myEmail, img }) => {
           <SubmitButton
             onClick={() => handleSubmit()}
             disabled={
-              !isValid.isName ||
-              !isValid.isEmail 
-              // !(checkName || checkEmail)
+              !(isValid.isName &&
+              isValid.isEmail &&
+              checkName &&
+              checkEmail)
             }
           >
             저장
@@ -191,6 +202,8 @@ const InputBox = styled.div`
   .error {
     color: #e45151;
     font-size: 13px;
+    position: relative;
+    bottom: 30px;
   }
 `;
 const ButtonBox = styled.div`
@@ -198,16 +211,22 @@ const ButtonBox = styled.div`
   flex-direction: column;
   align-items: center;
 `;
-const Img = styled.img`
+const Img = styled.div`
   width: 95px;
   height: 95px;
-  margin: 10px auto;
+  margin-left: 112.5px;
+
   overflow: hidden;
+
   display: flex;
   justify-content: center;
   align-items: center;
+
   border-radius: 1000000000px;
-  box-shadow: 0 0 0 2.3px #ffffff, 0 0 0 4.6px ${Palette.point_blue};
+  box-shadow: 0 0 0 2.3px #ffffff, 0 0 0 4.6px ${Palette["point_blue"]};
+  img {
+    width: 100%
+  }
 `;
 const StyledButton = styled.button`
   border-radius: 50px;
@@ -222,9 +241,8 @@ const StyledButton = styled.button`
   bottom: 37.5px;
   left: 238px;
   cursor: pointer;
-  &:hover{
-    background-color: #8C9CCF;
-  }
+  background-color: ${(props) =>
+    props.disabled ? Palette.button_gray : Palette.button_blue};
 `;
 const SubmitButton = styled.button`
   margin-top: 20px;
